632b0b2a63b96ecb43ccda5a8aec17a08988939e
William Donnelly <wdonnell@c1r3p4.hive.fi>
2021-11-05 15:54:12 +0200

    added tests to atoi

--- test_ft_atoi.c
+++ test_ft_atoi.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   test_ft_atoi.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: parkharo <parkharo@student.hive.fi>        +#+  +:+       +#+        */
+/*   By: wdonnell <wdonnell@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/02 21:14:42 by parkharo          #+#    #+#             */
-/*   Updated: 2021/11/03 13:53:11 by parkharo         ###   ########.fr       */
+/*   Updated: 2021/11/05 15:52:44 by wdonnell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -103,4 +103,59 @@ void test_ft_atoi()
 			TESTOK(8);
 	else
 			TESTFAILED(8);
+			
+	TESTN(9);
+	printf("--42");
+	i = atoi("--42");
+	ii = ft_atoi("--42");
+	printf("\nOriginal libc function returns %d\n", i);
+	printf("\nFunction to  be tested returns %d\n", ii);
+	if (i == ii)
+			TESTOK(7);
+	else
+			TESTFAILED(7);
+
+	TESTN(10);
+	printf("");
+	i = atoi("");
+	ii = ft_atoi("");
+	printf("\nOriginal libc function returns %d\n", i);
+	printf("\nFunction to  be tested returns %d\n", ii);
+	if (i == ii)
+			TESTOK(7);
+	else
+			TESTFAILED(7);
+	
+	TESTN(11);
+	printf("4+2");
+	i = atoi("4+2");
+	ii = ft_atoi("4+2");
+	printf("\nOriginal libc function returns %d\n", i);
+	printf("\nFunction to  be tested returns %d\n", ii);
+	if (i == ii)
+			TESTOK(7);
+	else
+			TESTFAILED(7);
+
+	TESTN(12);
+	printf("12345678909");
+	i = atoi("12345678909");
+	ii = ft_atoi("12345678909");
+	printf("\nOriginal libc function returns %d\n", i);
+	printf("\nFunction to  be tested returns %d\n", ii);
+	if (i == ii)
+			TESTOK(7);
+	else
+			TESTFAILED(7);
+	
+	TESTN(13);
+	printf("fourty two");
+	i = atoi("fourty two");
+	ii = ft_atoi("fourty two");
+	printf("\nOriginal libc function returns %d\n", i);
+	printf("\nFunction to  be tested returns %d\n", ii);
+	if (i == ii)
+			TESTOK(7);
+	else
+			TESTFAILED(7);
 }
